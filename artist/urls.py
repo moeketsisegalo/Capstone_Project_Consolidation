5df89d9f6e669a8dc092be73a56c0a5342ab66f1
Moeketsi <moeketsisegalo@gmail.com>
2023-05-20 23:39:31 +0200

    Add docstrings to urls.py

--- artist/urls.py
+++ artist/urls.py
@@ -5,21 +5,63 @@ from django.contrib.auth import views as auth_views
 # Define the URL patterns
 
 urlpatterns = [
-    # Home page
     path('', views.home, name='home'),
+    """
+    The home page URL pattern.
+    
+    :param: None
+    :type: None
+    :return: The rendered home page.
+    :rtype: HttpResponse
+    """
 
-    # Albums page
     path('albums/', views.albums, name='albums'),
+    """
+    The albums page URL pattern.
+    
+    :param: None
+    :type: None
+    :return: The rendered albums page.
+    :rtype: HttpResponse
+    """
 
-    # Tour page
     path('tours/', views.tour, name='tours'),
+    """
+    The tours page URL pattern.
+    
+    :param: None
+    :type: None
+    :return: The rendered tours page.
+    :rtype: HttpResponse
+    """
 
-    # User registration page
     path('register/', views.register, name='register'),
+    """
+    The user registration page URL pattern.
+    
+    :param: None
+    :type: None
+    :return: The rendered user registration page.
+    :rtype: HttpResponse
+    """
 
-    # Login page
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    """
+    The login page URL pattern.
+    
+    :param: template_name: The template to use for rendering the login page.
+    :type: str
+    :return: The rendered login page.
+    :rtype: HttpResponse
+    """
 
-    # Logout page
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    """
+    The logout page URL pattern.
+    
+    :param: None
+    :type: None
+    :return: The rendered logout page.
+    :rtype: HttpResponse
+    """
 ]
