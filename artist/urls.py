4116c97e6ea3217e88da51980158983c7cc72830
moeketsisegalo@gmail.com <118819421+moeketsisegalo@users.noreply.github.com>
2023-05-21 13:51:24 +0200

    Update urls.py

--- artist/urls.py
+++ artist/urls.py
@@ -4,62 +4,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    """
-    The home page URL pattern.
-    
-    :param: None
-    :type: None
-    :return: The rendered home page.
-    :rtype: HttpResponse
-    """
-
     path('albums/', views.albums, name='albums'),
-    """
-    The albums page URL pattern.
-    
-    :param: None
-    :type: None
-    :return: The rendered albums page.
-    :rtype: HttpResponse
-    """
-
     path('tours/', views.tour, name='tours'),
-    """
-    The tours page URL pattern.
-    
-    :param: None
-    :type: None
-    :return: The rendered tours page.
-    :rtype: HttpResponse
-    """
-
     path('register/', views.register, name='register'),
-    """
-    The user registration page URL pattern.
-    
-    :param: None
-    :type: None
-    :return: The rendered user registration page.
-    :rtype: HttpResponse
-    """
-
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    """
-    The login page URL pattern.
-    
-    :param: template_name: The template to use for rendering the login page.
-    :type: str
-    :return: The rendered login page.
-    :rtype: HttpResponse
-    """
-
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    """
-    The logout page URL pattern.
-    
-    :param: None
-    :type: None
-    :return: The rendered logout page.
-    :rtype: HttpResponse
-    """
 ]
+
